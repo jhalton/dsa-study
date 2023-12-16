788e998af423ca8f3c076bb519f075565db8c47a
Jeanette <haltonjeanette@gmail.com>
2023-12-16 10:29:45 -0800

    Completed hackerrank lonelyInteger

--- Hackerrank/preperation-kit.js
+++ Hackerrank/preperation-kit.js
@@ -18,3 +18,22 @@ function matchingStrings(strings, queries) {
   }
   return res;
 }
+
+//---------------------12.16.23---------------------
+
+//My thought process was to create an object to hold the keys and count of how many times
+//that integer appears.
+//Then, access that key by turning that object into an array of keys and using
+//the built-in filter method to find the find the key whose value is equal to 1
+function lonelyinteger(a) {
+  const intObj = {};
+
+  a.forEach((int) => {
+    if (int in intObj) {
+      intObj[int] += 1;
+    } else {
+      intObj[int] = 1;
+    }
+  });
+  return Object.keys(intObj).find((key) => intObj[key] === 1);
+}
