6392fd29da0b11be75bd6ba846fb05b0c402be89
Jeanette <haltonjeanette@gmail.com>
2023-12-18 02:15:28 -0800

    Leetcode isValid

---
+++ Leetcode/algo-academy.js
@@ -0,0 +1,28 @@
+//----------------12.18.23---------------------
+
+//Use a stack to determine whether parentheses are positioned in the correct spots.
+//We're using a hash to determine whether the character is matched with its corresponding
+//parenthesis. If not, return false.
+var isValid = function (s) {
+  if (s.length % 2 !== 0) return false;
+
+  const stack = [];
+
+  const closeToOpen = {
+    ")": "(",
+    "}": "{",
+    "]": "[",
+  };
+
+  for (let i = 0; i < s.length; i++) {
+    let char = s[i];
+    if (!closeToOpen[char]) {
+      stack.push(char);
+    } else if (!stack.length || stack[stack.length - 1] !== closeToOpen[char]) {
+      return false;
+    } else {
+      stack.pop();
+    }
+  }
+  return stack.length === 0;
+};
