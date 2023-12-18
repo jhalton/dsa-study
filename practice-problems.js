12b5ae3df79d130832b51e3aa9c56745c525d20a
Jeanette <haltonjeanette@gmail.com>
2023-12-18 04:56:04 -0800

    Structy compress

--- practice-problems.js
+++ practice-problems.js
@@ -38,7 +38,8 @@ const isPrime = (n) => {
 */
 const uncompress = (s) => {
   const nums = "0123456789";
-  let res = "";
+  // let res = "";
+  let res = [];
   let i = 0;
   let j = 0;
 
@@ -48,11 +49,34 @@ const uncompress = (s) => {
     } else {
       const num = Number(s.slice(i, j));
       for (let count = 0; count < num; count++) {
-        res += s[j];
+        // res += s[j];
+        res.push(s[j]);
       }
       j++;
       i = j;
     }
   }
-  return res;
+  // return res;
+  return res.join("");
+};
+
+//-----------------------------------------------
+
+//In the walk-through, Alvin uses pointers again. I went with a for loop because
+//it makes more sense for me to keep track of fewer pointers. But pointers are good
+//to use. Maybe I will come back through later and try it a different way.
+const compress = (s) => {
+  const res = [];
+  let count = 1;
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === s[i + 1]) {
+      count++;
+    } else {
+      if (count > 1) res.push(count);
+      res.push(s[i]);
+      count = 1;
+    }
+  }
+  return res.join("");
 };
