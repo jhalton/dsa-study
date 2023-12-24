f6a1997ca41b50487f8f722b4bdee1f0dff2b64b
Jeanette <haltonjeanette@gmail.com>
2023-12-23 21:55:41 -0800

    Structy linked lists walkthrough

--- practice-problems.js
+++ practice-problems.js
@@ -274,4 +274,43 @@ const fiveSort = (nums) => {
 
 //The Structy solution is about the same as mine, just structured a little bit differently
 
-//--------------------------12.24.23-------------------------------
+//--------------------------12.28.23-------------------------------
+
+//Linked Lists
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+const a = new Node("A");
+const b = new Node("B");
+const c = new Node("C");
+const d = new Node("D");
+
+a.next = b;
+b.next = c;
+c.next = d;
+
+//      A -> B -> C -> D -> NULL
+//      cur
+
+//ITERATIVE
+// const printLinkedList = (head) => {
+//   let current = head;
+//   while (current !== null) {
+//     console.log(current.val);
+//     current = current.next;
+//   }
+// };
+
+//RECURSIVE
+const printLinkedList = (head) => {
+  if (head === null) return;
+
+  console.log(head.val);
+  printLinkedList(head.next);
+};
+
+printLinkedList(a);
