054b9eddf160644d5495e4d7149a25440bfa6f1e
Jeanette <haltonjeanette@gmail.com>
2023-12-23 19:21:15 -0800

    Udemy playwright course, javascript review

---
+++ Udemy/Playwright/index.js
@@ -0,0 +1,15 @@
+//This is part of the javascript review
+let familySize = 2;
+let plannedDistanceToDrive = 100;
+
+function recommendedCar(familySize, plannedDistanceToDrive) {
+  if (familySize <= 4 && plannedDistanceToDrive < 200) {
+    return "Tesla";
+  } else if (familySize <= 4 && plannedDistanceToDrive >= 200) {
+    return "Toyota Camry";
+  } else {
+    return "Minivan";
+  }
+}
+
+console.log(recommendedCar(familySize, plannedDistanceToDrive));
